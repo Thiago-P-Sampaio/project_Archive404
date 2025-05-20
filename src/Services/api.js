51f25d322fa36bfb937e8f4e5a1433c69d337dc5
Thiago P <thiago.p.sampaio07@gmail.com>
2025-05-20 16:07:49 -0300

    Alterando a BASEURL para a do deploy do back

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const  gamesService = axios.create({
-    baseURL: 'http://192.168.1.170:8082/games'
+    baseURL: 'https://404archive.azurewebsites.net/jogos'
 })
 
 
